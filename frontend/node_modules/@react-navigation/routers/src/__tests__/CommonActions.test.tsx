12511ff29204a10816bab83c2109e407d693779b
Manuel Lang <manuellang183@gmail.com>
2020-04-25 01:07:44 +0200

    removed node_modules and package-lock

--- frontend/node_modules/@react-navigation/routers/src/__tests__/CommonActions.test.tsx
+++
@@ -1,8 +0,0 @@
-import * as CommonActions from '../CommonActions';
-
-it('throws if NAVIGATE is called without key or name', () => {
-  // @ts-ignore
-  expect(() => CommonActions.navigate({})).toThrowError(
-    'You need to specify name or key when calling navigate with an object as the argument.'
-  );
-});
--- frontend/node_modules/@react-navigation/web/src/index.js
+++
@@ -1,3 +0,0 @@
-export { default as createBrowserApp } from './createBrowserApp';
-export { default as handleServerRequest } from './handleServerRequest';
-export { default as Link } from './Link';
