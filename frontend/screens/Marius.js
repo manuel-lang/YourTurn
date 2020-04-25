dd190e19376b19352460b841ecd90080b67bea02
Marius Bauer <mariusbauer91@gmail.com>
2020-04-26 01:46:12 +0200

    made the category buttons sexy ;-)

--- frontend/screens/Marius.js
+++ frontend/screens/Marius.js
@@ -1,23 +1,21 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, Image, TextInput } from 'react-native';
+import { StyleSheet, View, ScrollView } from 'react-native';
 import { FlatList } from "react-native-gesture-handler";
-import { Button } from 'galio-framework';
-import {Button as B} from 'react-native';
+import { Button } from 'react-native-material-ui';
 import FeedItem  from './FeedItem';
 import Minhkha from './Minhkha';
-import { Ionicons } from '@expo/vector-icons';
-import Colors from "../constants/Colors";
+
+let color_button_inactive = '#ababab';
+let color_button_active = '#443f3c';
+let background = '#2d2d2d';
+let bc_button_active = '#ffc491';
+let bc_button_inactive = background
+let font = '#ebebeb'
 
 const CustomButton = (props) => {
     const [isActive, setIsActive] = useState(false)
 
     const onPressCustomButton = () => {
-
-        // enthält alle aktiven Tags
-        // activeTags={activeTags}
-        // zum setzen der Tags
-        // activeTagsFunc={setActiveTags}
-
         if (!isActive) {
             props.activeTags.push(props.tag)
         } else {
@@ -47,14 +45,18 @@ const CustomButton = (props) => {
                 .catch((error) => console.error(error))
     }
 
+    let style_button_inactive = {container: {marginLeft: 10, borderRadius: 25, height: 30, backgroundColor: bc_button_inactive},
+                                 text:      {fontSize: 18, color: color_button_inactive}};
+    let style_button_active   = {container: {marginLeft: 10, borderRadius: 25, height: 40, backgroundColor: bc_button_active},
+                                 text:      {fontSize: 18, color: color_button_active}};
     return (
         <Button
-            style={{flex: 1, margin: 5, }}
+            upperCase={false}
+            text={props.tag}
+            style={(isActive ? style_button_active : style_button_inactive)}
             onPress = {onPressCustomButton}
-            color={isActive ? 'red' : 'orange'}
-        >
-            {props.tag}
-        </Button>
+            onLongPress= {onPressCustomButton}
+        />
     );
 }
 
@@ -78,14 +80,30 @@ function Marius() {
     }, []);
 
 
+
     return (
-        <View style={{flex: 1}}>
+        <View style={{flex: 1, backgroundColor: background}}>
             {!showDetails &&
             <View style={styles.wrapper}>
                 <View style={styles.orderButtons}>
-                    <CustomButton tag="Health" func={setfetchedData} activeTags={activeTags} />
-                    <CustomButton tag="Social" func={setfetchedData} activeTags={activeTags} />
-                    <CustomButton tag="Lifestyle" func={setfetchedData} activeTags={activeTags} />
+                    {/*<FlatList
+                        data={[
+                            {key: "Health"},
+                            {key: "Social"},
+                            {key: "Lifestyle"}
+                        ]}
+                        horizontal={true}
+                        renderItem={({ item }) => <CustomButton tag={item.key} func={setfetchedData} activeTags={activeTags} />}
+                        keyExtractor={item => item.id}
+                    />*/}
+                    <ScrollView horizontal={true} style={styles.cb_scrollview} contentContainerStyle={{alignItems: 'center'}}>
+                        <CustomButton tag="Health" func={setfetchedData} activeTags={activeTags} />
+                        <CustomButton tag="Sustainable" func={setfetchedData} activeTags={activeTags} />
+                        <CustomButton tag="Sport" func={setfetchedData} activeTags={activeTags} />
+                        <CustomButton tag="Creativity" func={setfetchedData} activeTags={activeTags} />
+                        <CustomButton tag="Location" func={setfetchedData} activeTags={activeTags} />
+                        <CustomButton tag="International" func={setfetchedData} activeTags={activeTags} />
+                    </ScrollView>
                 </View>
                 <View style={{flex: 11, justifyContent: 'space-between'}}>
                     <FlatList
@@ -121,9 +139,15 @@ const styles = StyleSheet.create({
         fontFamily: 'Arial'
     },
     orderButtons: {
-        flex: 1,
+        flex: 1.5,
+        justifyContent: 'space-evenly',
         alignItems: 'center',
-        flexDirection: 'row'
+        flexDirection: 'row',
+    },
+    cb_scrollview: {
+        paddingTop: 10,
+        paddingBottom: 10,
+        height: '100%',
     }
 });
 
