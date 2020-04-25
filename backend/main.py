ba22b6a74b49208f2f4637ea33f6571d87136108
Manuel Lang <manuellang183@gmail.com>
2020-04-25 11:16:12 +0200

    backend cleanup

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 from fastapi import FastAPI, status
+from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
 from models import Challenge, Notification, User
 import os
@@ -11,30 +12,13 @@ app = FastAPI()
 load_dotenv()
 
 
-def jsonify(func):
-    """
-    Creates a JSON response based on a status code and return data.
-    :param func: the individual function
-    :return: a wrapper that catches exceptions and gives a JSON response
-    """
-    def wrapper(*args, **kwargs):
-        try:
-            status_code, items = func(*args, **kwargs)
-            return JSONResponse(status_code=status_code, content=items)
-        except Exception as e:
-            return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, content=e)
-
-    return wrapper
-
-
 def _get_db():
     return MongoClient(f'mongodb://{os.getenv("USR_")}:{os.getenv("PWD_")}@{os.getenv("REMOTE_HOST")}:'
                        f'{os.getenv("REMOTE_PORT")}/{os.getenv("AUTH_DB")}')[os.getenv("MAIN_DB")]
 
 
 @app.get("/challenges")
-@jsonify
-def get_challenges(user_id: int = None) -> Tuple[any, list]:
+def get_challenges(user_id: int) -> JSONResponse:
     """
     Lists all challenges.
     :param user_id: the id of the user
@@ -43,112 +27,106 @@ def get_challenges(user_id: int = None) -> Tuple[any, list]:
     db = _get_db()
     all_challenges = list(db["challenges"].find())
     if not user_id:
-        return status.HTTP_200_OK, all_challenges
+        return JSONResponse(status_code=status.HTTP_200_OK, content=jsonable_encoder(all_challenges))
     else:
-        bookmarked_challenges = list(db["users"].find({"user_id": user_id})["bookmarks"])
+        bookmarked_challenges = [x["bookmarks"] for x in list(db["users"].find({"user_id": user_id}))]
+        other_challenges = [x for x in all_challenges if
+                            x["owner_id"] != user_id and x["challenge_id"] not in bookmarked_challenges]
+        random.shuffle(other_challenges)
         ordered_challenges = [x for x in all_challenges if x["owner_id" == user_id]] + [x for x in all_challenges if x[
-            "challenge_id"] in bookmarked_challenges] + random.shuffle(
-            [x for x in all_challenges if x["owner_id"] != user_id and x["challenge_id"] not in bookmarked_challenges])
-        return status.HTTP_200_OK, ordered_challenges
+            "challenge_id"] in bookmarked_challenges] + other_challenges
+        return JSONResponse(status_code=status.HTTP_200_OK, content=jsonable_encoder(ordered_challenges))
 
 
 @app.post("/challenges")
-@jsonify
-def create_challenge(challenge: Challenge) -> Tuple[any, list]:
+def create_challenge(challenge: Challenge) -> JSONResponse:
     """
     Adds a new challenge.
     :param challenge: the challenge information
     :return: status code and response data
     """
     _get_db()["challenges"].insert_one(challenge)
-    return status.HTTP_201_CREATED, []
+    return JSONResponse(status_code=status.HTTP_201_CREATED, content=jsonable_encoder([]))
 
 
 @app.put("/challenges")
-@jsonify
-def adjust_challenge(challenge: Challenge) -> Tuple[any, list]:
+def adjust_challenge(challenge: Challenge) -> JSONResponse:
     """
     Adjusts a challenge.
     :param challenge: the challenge information
     :return: status code and response data
     """
     _get_db()["challenges"].update_one({"challenge_id": challenge.challenge_id}, {"$set": challenge})
-    return status.HTTP_204_NO_CONTENT, []
+    return JSONResponse(status_code=status.HTTP_204_NO_CONTENT, content=jsonable_encoder([]))
+
 
 
 @app.get("/challenges/{challenge_id}")
-@jsonify
-def get_individual_challenge(challenge_id: int) -> Tuple[any, list]:
+def get_individual_challenge(challenge_id: int) -> JSONResponse:
     """
     Lists the information for a specific challenge.
     :param challenge_id: the id of the challenge
     :return: status code and response data
     """
     challenge = _get_db()["challenges"].find_one({"challenge_id": challenge_id})
-    return status.HTTP_200_OK, [challenge]
+    return JSONResponse(status_code=status.HTTP_200_OK, content=jsonable_encoder([challenge]))
 
 
 @app.get("/users")
-@jsonify
-def get_users() -> Tuple[any, list]:
+def get_users() -> JSONResponse:
     """
     Lists all users.
     :return: status code and response data
     """
     users = list(_get_db()["users"].find())
-    return status.HTTP_200_OK, users
+    return JSONResponse(status_code=status.HTTP_200_OK, content=jsonable_encoder(users))
 
 
 @app.post("/users")
-@jsonify
-def create_user(user: User) -> Tuple[any, list]:
+def create_user(user: User) -> JSONResponse:
     """
     Adds a new user.
     :param user: the update information
     :return: status code and response data
     """
     _get_db()["users"].insert_one(user)
-    return status.HTTP_201_CREATED, []
+    return JSONResponse(status_code=status.HTTP_201_CREATED, content=jsonable_encoder([]))
 
 
 @app.put("/users")
-@jsonify
-def adjust_user(user: User) -> Tuple[any, list]:
+def adjust_user(user: User) -> JSONResponse:
     """
     Lists all information belonging to one user.
     :param user: the update information
     :return: status code and response data
     """
     _get_db()["users"].update_one({"user_id": user.user_id}, {"$set": user})
-    return status.HTTP_204_NO_CONTENT, []
+    return JSONResponse(status_code=status.HTTP_204_NO_CONTENT, content=jsonable_encoder([]))
 
 
 @app.get("/users/{user_id}")
-@jsonify
-def get_individual_user(user_id: int) -> Tuple[any, list]:
+def get_individual_user(user_id: int) -> JSONResponse:
     """
     Lists all information belonging to one user.
     :param user_id: the id of the user
     :return: status code and response data
     """
     user = _get_db()["users"].find_one({"user_id": user_id})
-    return status.HTTP_200_OK, [user]
+    return JSONResponse(status_code=status.HTTP_200_OK, content=jsonable_encoder([user]))
 
 
 @app.get("/notifications")
-@jsonify
-def get_user_notificatiions(user_id: int = None) -> Tuple[any, list]:
+def get_user_notifications(user_id: int = None) -> JSONResponse:
     """
     Lists all notifications for a certain user
     :param user_id: the id of the user
     :return: status code and response data
     """
     notifications = list(_get_db()["notifications"].find({"user_id": user_id}))
-    return status.HTTP_200_OK, notifications
+    return JSONResponse(status_code=status.HTTP_200_OK, content=jsonable_encoder(notifications))
 
 
 @app.post("/notifications")
-@jsonify
-def create_notification(notification: Notification) -> Tuple[any, list]:
+def create_notification(notification: Notification) -> JSONResponse:
     _get_db()["notifications"].insert_one(notification)
-    return status.HTTP_201_CREATED, []
+    return JSONResponse(status_code=status.HTTP_201_CREATED, content=jsonable_encoder([]))
