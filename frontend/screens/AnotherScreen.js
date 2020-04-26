ceb1e02ac9c21458677e927c134a6121d719a56d
CiiDyY <wundix.acc@gmail.com>
2020-04-26 18:24:08 +0200

    sexy asian removed

--- frontend/screens/AnotherScreen.js
+++ frontend/screens/AnotherScreen.js
@@ -4,143 +4,35 @@ import * as React from 'react';
 import { StyleSheet, Text, View, Image, TextInput } from 'react-native';
 import { RectButton, ScrollView } from 'react-native-gesture-handler';
 
-import { Button } from 'react-native-material-ui';
-import { Toolbar } from 'react-native-material-ui';
-import { Snackbar } from 'react-native-material-ui';
 import Collapsible from 'react-native-collapsible';
-import { Card } from 'react-native-material-ui';
 
+import { Button } from 'react-native-material-ui';
+import { Checkbox } from 'react-native-material-ui'
+import { Badge, Icon, Avatar } from 'react-native-material-ui';
+import { RadioButton } from 'react-native-material-ui';
+import { ActionButton } from 'react-native-material-ui';
 
-export default function AnotherScreen() {
+import Colors from '../constants/Colors';
+
+export default function OwnScreen() {
 
   const [showPic, setShowPic] = React.useState(true);
   const [collapseOpen, setCollapseOpen] = React.useState(false);
-  const [snackBarVisible, setSnackBarVisible] = React.useState(false);
+  const [isChecked, setIsChecked] = React.useState(false);
 
   return (
     <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-
-      <View 
-        style={styles.cardContainer}
-      >
-        <View
-         style={styles.card}
-        >
-          <Card
-            onPress={() => setCollapseOpen(!collapseOpen)}
-          >
-            <Image 
-              source={require('../assets/images/profile_picture.jpg') }
-              style={{width: "100%", height: 100}}
-              // style={{width: "100%", height: (!collapseOpen ? 100 : 400)}}
-            />
-            <View style={styles.cardText}>
-              <Text style={{marginBottom: 10, fontWeight: "bold", fontSize: 20}}>Hello world!</Text>
-              <Text style={{fontSize: 15}}>Sexy asian alert</Text>
-              <Text style={{fontSize: 15}}>Sexy asian alert</Text>
-
-              <Collapsible  collapsed={!collapseOpen}>
-                <Text style={{fontSize: 15}}>"Wow"</Text>
-                <Text style={{fontSize: 15}}>"Totally agree"</Text>
-                <Text style={{fontSize: 15}}>More information 3</Text>
-                <Text style={{fontSize: 15}}>More information 4</Text>
-
-                <Button 
-                  accent 
-                  text="Contact" 
-                  onPress={() => console.log("contact")}
-                /> 
-
-              </Collapsible >
-            </View>
-          </Card>
-        </View>
-
-        <View
-         style={styles.card}
-        >
-          <Card>
-            <Image 
-              source={require('../assets/images/profile_picture.jpg') }
-              style={{width: "100%", height: 100}}
-            />
-            <View style={styles.cardText}>
-              <Text style={{marginBottom: 10, fontWeight: "bold", fontSize: 20}}>Hello world!</Text>
-              <Text style={{fontSize: 15}}>Sexy asian alert</Text>
-              <Text style={{fontSize: 15}}>Sexy asian alert</Text>
-            </View>
-          </Card>
-        </View>
-
-        <View
-         style={styles.card}
-        >
-          <Card>
-            <Image 
-              source={require('../assets/images/profile_picture.jpg') }
-              style={{width: "100%", height: 100}}
-            />
-            <View style={styles.cardText}>
-              <Text style={{marginBottom: 10, fontWeight: "bold", fontSize: 20}}>Hello world!</Text>
-              <Text style={{fontSize: 15}}>Sexy asian alert</Text>
-              <Text style={{fontSize: 15}}>Sexy asian alert</Text>
-            </View>
-          </Card>
-        </View>
-
-      </View>
-
     </ScrollView>
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fafafa',
-  },
-  cardContainer: {
-    alignItems: 'center',
-    marginBottom: 10,
-  },
-  card: {
-    width: "100%",
-    padding: 5,
-  }, 
-  cardText: {
-    padding: 10,
-  }, 
-  buttonContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: "space-evenly",
-    margin: 10,
-  },
-  button: {
-    backgroundColor: 'green',
-    width: '40%',
-    height: 40
+    backgroundColor: Colors.backgroundColorLight,
   },  
   contentContainer: {
     paddingTop: 15,
-  },
-  optionIconContainer: {
-    marginRight: 12,
-  },
-  option: {
-    backgroundColor: '#fdfdfd',
-    paddingHorizontal: 15,
-    paddingVertical: 15,
-    borderWidth: StyleSheet.hairlineWidth,
-    borderBottomWidth: 0,
-    borderColor: '#ededed',
-  },
-  lastOption: {
-    borderBottomWidth: StyleSheet.hairlineWidth,
-  },
-  optionText: {
-    fontSize: 15,
-    alignSelf: 'flex-start',
-    marginTop: 1,
   },
 });
